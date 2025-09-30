07f7995fc22c6efab55abe8aa102887c13db4d6b
def-turtle <turtle48sigma@gmail.com>
2025-09-30 20:45:44 +0200

    fixed_main.py

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import random
 a=random.randint(1,10)
-b=int(input("Enter a number between 1 and 10"))
-if a==b:
-    print("You are right")
-else:
-    print("You are wrong")
-    print("The number was", a)
-#hello
+c=6
+for i in range(1,6):
+    c-=1
+    print("You have", c ,"tries left")
+    b = int(input("Enter a number between 1 and 10: "))
+    if a == b:
+        print("You guessed right")
+        break
+    else:
+        print("You are wrong")
+    if c == 1:
+        print("You didn't guess right, the right number was", a)
+        break
