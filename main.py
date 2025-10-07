17498d5740f546da77d07ed657e2c2a6179c0cf0
def-turtle <turtle48sigma@gmail.com>
2025-10-07 16:10:39 +0200

    added a program that reverses your number in main.py

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-import random
-a=random.randint(1,10)
-c=6
-for i in range(1,6):
-    c-=1
-    print("You have", c ,"tries left")
-    b = int(input("Enter a number between 1 and 10: "))
-    if a == b:
-        print("You guessed right")
-        break
-    else:
-        print("You are wrong")
-    if c == 1:
-        print("You didn't guess right, the right number was", a)
-        break
+# import random
+# a=random.randint(1,10)
+# c=6
+# for i in range(1,6):
+#     c-=1
+#     print("You have", c ,"tries left")
+#     b = int(input("Enter a number between 1 and 10: "))
+#     if a == b:
+#         print("You guessed right")
+#         break
+#     else:
+#         print("You are wrong")
+#     if c == 1:
+#         print("You didn't guess right, the right number was", a)
+#         break
+import math
+a = int(input('Введіть 5-ти значне число: '))
+reversed_num = int(str(a)[::-1])
+b = len(str(a))
+if b == 5:
+    print(reversed_num)
+else:
+    print("Not a 5 digit number.")
