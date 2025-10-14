8cb5071960678f8e6b79b8ddc38a3c5562cce28b
def-turtle <turtle48sigma@gmail.com>
2025-10-14 11:40:19 +0200

    changed hw

--- Lesson_4_hw_1.py
+++ Lesson_4_hw_1.py
@@ -1,10 +1,18 @@
-user_input = int(input("Enter a few number: "))
-list1 = list(map(int, str(user_input)))
-lastItem = list1[-1]
-answer = 0
+import random
+amount = random.randint(3,10)
+listShow = []
+for i in range(amount):
+    randomNumber = random.randint(0, 100)
+    listShow.append(randomNumber)
+    i+=1
+    # random_list = random.randint(0, 100)
+    # if i==0:
+    #     print("[",random_list, end=",", sep="")
+    #     continue
+    # elif i==9:
+    #     print(random_list, end="]", sep="")
+    #     continue
+    # else:
+    #     print(random_list,end=",", sep="")
 
-for index, x in enumerate(list1):
-    if index % 2 == 0:
-        answer += x * lastItem
-
-print(answer)
+print(listShow)
--- Lesson_4_hw_2.py
+++ Lesson_4_hw_2.py
@@ -1,12 +1,8 @@
 user_input = int(input("Enter a few number: "))
 list1 = list(map(int, str(user_input)))
 lastItem = list1[-1] #6
-answer = 0
-for x in list1:
-    index1 = list1.index(x)
-    even_index = index1 % 2 == 0
-    if even_index:
-        answer += x*lastItem
-    # if x.__index__()%2==0:
-    #     newList.append(x*lastItem)
-print(answer)
+answear = 0
+for index,x in enumerate(list1):
+    if index%2 ==0:
+        answear += x*lastItem
+print(answear)
