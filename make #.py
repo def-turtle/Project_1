943d3d9f31b016df717397d4703ea96f34f35a11
def-turtle <turtle48sigma@gmail.com>
2025-10-19 09:40:40 +0200

    Покращив #

--- make #.py
+++ make #.py
@@ -1,31 +1,12 @@
 a = input("Please enter something: ")
-b = ''
-split = a.split(" ")
-for i in split:
-    if i[0].islower():
-        upper = i[0].upper() + i[1:]
-        b+=upper
-        continue
-    b+=i
-print(b)
+b = a.title()
 clean = ''
+list1 = [",",".","?","!","1","2","3","4","5","6","7","8","9","0"]
 clean = b.replace(" ", "")
-clean = clean.replace(",", "")
-clean = clean.replace("!", "")
-clean = clean.replace("?", "")
-clean = clean.replace("1", "")
-clean = clean.replace("2", "")
-clean = clean.replace("3", "")
-clean = clean.replace("4", "")
-clean = clean.replace("5", "")
-clean = clean.replace("6", "")
-clean = clean.replace("7", "")
-clean = clean.replace("8", "")
-clean = clean.replace("9", "")
-clean = clean.replace("0", "")
+for i in list1:
+    clean = clean.replace(i,"")
 clean = "#"+clean
-lenght = len(clean)
-
-if lenght > 140:
+lenght1 = len(clean)
+if lenght1 > 140:
     clean = clean[:140]
 print(clean)
