c913f186be137560f9eed3c1ad92a23f8a1f7460
def-turtle <turtle48sigma@gmail.com>
2025-10-21 15:42:59 +0200

    int name checker

--- Lesson_4_hw_4.py
+++ Lesson_4_hw_4.py
@@ -1,8 +1,8 @@
 import math
-list1 = [1,2,3,4,5,6,232]
+list1 = [1,2,3]
 list1_len = len(list1)
 amount = math.ceil(list1_len/2)
 list2 = list1[amount:]
 list3 = list1[:amount]
-new_list = [list2,list3]
+new_list = [list3,list2]
 print(new_list)
