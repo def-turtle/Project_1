e5ad96f6aa7a6b29c3bd10ba70c92e67ba78e6d5
def-turtle <turtle48sigma@gmail.com>
2025-10-24 21:33:30 +0200

    fixed find_unique_value

--- find_unique_value.py
+++ find_unique_value.py
@@ -3,10 +3,13 @@ def find_unique_value(some_list:list):
     for item in some_list:
         if some_list.count(item)==1:
             unique_item = item
+        else:
+            pass
     print(unique_item)
     return unique_item
 find_unique_value([1,2,1,1])
 assert find_unique_value([1, 2, 1, 1]) == 2, 'Test1'
 assert find_unique_value([2, 3, 3, 3, 5, 5]) == 2, 'Test2'
 assert find_unique_value([5, 5, 5, 2, 2, 0.5]) == 0.5, 'Test3'
+assert find_unique_value([2,2,2]) == 0, 'Test4'
 print("ОК")
