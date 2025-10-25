6989f090a111d0c892f9c398049958442134c80f
def-turtle <turtle48sigma@gmail.com>
2025-10-25 15:41:12 +0200

    is_palindrome

---
+++ is_palindrome.py
@@ -0,0 +1,20 @@
+import string
+def is_palindrome(text:str):
+    banned = string.punctuation
+    for i in banned:
+        if i in text:
+            text = text.replace(i,"")
+    if " " in text:
+        text = text.replace(" ","")
+    text = text.lower()
+    text_l = list(text)
+    if text_l[::-1] == list(text):
+       return True
+    else:
+        return False
+is_palindrome('A man, a plan, a canal: Panama')
+assert is_palindrome('A man, a plan, a canal: Panama') == True, 'Test1'
+assert is_palindrome('0P') == False, 'Test2'
+assert is_palindrome('a.') == True, 'Test3'
+assert is_palindrome('aurora') == False, 'Test4'
+print("ОК")
