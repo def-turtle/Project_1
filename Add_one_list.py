d1c0e1b5e8bb76aeb6df815a82fedfa688142cc3
def-turtle <turtle48sigma@gmail.com>
2025-10-28 16:01:27 +0100

    add 1 to list

--- Add_one_list.py
+++ Add_one_list.py
@@ -3,7 +3,9 @@ def add_one(some_list):
     new_list = list(map(int, str(int_result)))
     return new_list
     print(new_list)
+
 add_one([1,2,3,4])
+
 assert add_one([1, 2, 3, 4]) == [1, 2, 3, 5], 'Test1'
 assert add_one([9, 9, 9]) == [1, 0, 0, 0], 'Test2'
 assert add_one([0]) == [1], 'Test3'
