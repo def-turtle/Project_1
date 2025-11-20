b2e31751930cdb43de8c5394f6a79bb3d6fc2f7d
def-turtle <turtle48sigma@gmail.com>
2025-11-20 20:34:13 +0100

    made cleaner.py which cleanes any file from html code

---
+++ Rectangle.py
@@ -0,0 +1,39 @@
+class Rectangle:
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def get_square(self):
+        return self.width * self.height
+
+    def __eq__(self, other):
+        a = self.get_square()
+        b = other.get_square()
+        return a == b
+
+    def __add__(self, other):
+        total = self.get_square() + other.get_square()
+        height = total//self.width
+        return Rectangle(self.width, height)
+
+    def __mul__(self, n):
+        total = self.get_square() * n
+        height = total//self.width
+        return Rectangle(self.width, height)
+    def __str__(self):
+        return str(self.get_square())
+
+
+r1 = Rectangle(2, 4)
+r2 = Rectangle(3, 6)
+assert r1.get_square() == 8, 'Test1'
+assert r2.get_square() == 18, 'Test2'
+# print(r1+r2)
+r3 = r1 + r2
+assert r3.get_square() == 26, 'Test3'
+#
+r4 = r1 * 4
+assert r4.get_square() == 32, 'Test4'
+
+assert Rectangle(3, 6) == Rectangle(2, 9), 'Test5'
