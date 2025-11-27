e7ead708931eed95f1484fc65a872fcb5f2570db
def-turtle <turtle48sigma@gmail.com>
2025-11-27 15:22:39 +0100

    fixed Student_add_remove.py so that it would raise an error when the max amount is reached

--- Student_add_remove.py
+++ Student_add_remove.py
@@ -1,3 +1,10 @@
+students = {}
+
+
+class Limit_Error(Exception):
+    pass
+
+
 class Human:
 
     def __init__(self, gender, age, first_name, last_name):
@@ -8,7 +15,8 @@ class Human:
 
     def __str__(self):
         return f"{self.gender} {self.age} {self.first_name} {self.last_name}"
-students = {}
+
+
 class Student(Human):
     def __init__(self, gender, age, first_name, last_name, record_book):
         super().__init__(gender, age, first_name, last_name)
@@ -20,20 +28,24 @@ class Student(Human):
 
     def __str__(self):
         return f"{self.gender} {self.age} {self.first_name} {self.last_name}"
+
+
 class Group:
 
     def __init__(self, number):
         self.number = number
         self.amount = 0
         self.students = []
+
     def add(self, person):
         if len(self.students) >= 3:
             # print("You can't add more than 10 students")
-            return "you can't add more than 10 students."
+            raise Limit_Error
         if person in self.students:
             return None
         self.students.append(person)
         self.amount += 1
+
     def remove(self, last_name):
         student1 = None
         for student in self.students:
@@ -47,16 +59,20 @@ class Group:
         else:
             self.students.remove(student1)
             self.amount -= 1
+
     def find(self, last_name):
         for student in self.students:
             if student.last_name == last_name:
                 return student
         return None
+
     def __str__(self):
         students = []
         for student in self.students:
             students.append(str(student))
         return f"The Group number: {self.number}, Amount of people: {self.amount} students: {students}"
+
+
 st1 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
 st2 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
 st3 = Student('Male', 30, 'Michael', 'Jordan', 'AN142')
